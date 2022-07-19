b352ea824e25d29724d668368dda1aa84a2046bc
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-19 23:09:43 +0300

    update time

--- java.js
+++ java.js
@@ -1,3 +1,20 @@
+function formateDate(timestamp) {
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.get.Minutes();
+    if (minuts < 10){
+        minutes = `0${minutes}`}
+    let days =[ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+
+
+    return `${day}${hours}:${minutes}`
+}
+
+
 function displayTemperature (response){
     console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
@@ -10,7 +27,12 @@ cityElement.innerHTML = response.data.name;
 humidityElement.innerHTML = response.data.main.humidity;
  let windElement = document.querySelector("#wind");
 windElement.innerHTML = Math.round(response.data.wind.speed);
-}
+ let dateElement = document.querySelector("#;date");
+dateElement.innerHTML = formatDate(response.data.dt *1000);
+    
+    }
+
+
 let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
 let apiUrl =
   `https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=${apiKey}&units=metric`;
