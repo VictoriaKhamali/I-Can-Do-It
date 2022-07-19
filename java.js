23def34ec1144159c3eef50df6dcdc0da54570fd
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-19 22:26:44 +0300

    add new element city inner html

--- java.js
+++ java.js
@@ -2,6 +2,8 @@ function displayTemperature (response){
     console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
+     let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.name;
 }
 let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
 let apiUrl =
