9cfe43b0edf1aca39631590ae03dff591de30977
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-19 22:09:49 +0300

    add new function

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
+function displayTemperature (response){
+    console.log(response.data)
+}
 let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
 let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=${apiKey}`;
-  console.log(apiUrl)
+  `https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=${apiKey}&units=metric`;
+  
+  axios.get(url).then(displayTemperature);
