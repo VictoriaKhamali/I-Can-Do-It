595d2cd848022a67b1df049a756de3607e28fa76
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-21 09:38:49 +0300

    Update java.js

--- java.js
+++ java.js
@@ -37,7 +37,7 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 
      function search(city){
        let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
-       let city = "Kyiv";
+     
        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
      }
