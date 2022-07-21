415e32bd846589f1ace72b5d24a5e402628dc4e0
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-21 09:37:18 +0300

    Update java.js

--- java.js
+++ java.js
@@ -53,4 +53,5 @@ function handleSubmit(event){
 
 
   let form = document.querySelector("#search-form");
-  form.addEventListener("submit", handleSubmit)
+  form.addEventListener("submit", handleSubmit);
+  search("Kyiv");
