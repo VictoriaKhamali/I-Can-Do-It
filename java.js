15c7640bc209ce6e0f2b1b9e8b0cbb659401c03e
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-25 09:49:58 +0300

    api for each day forecast

--- java.js
+++ java.js
@@ -13,7 +13,8 @@ function formateDate(timestamp) {
 
     return `${day}, ${hours}:${minutes}`
 }
-function displayForecast () {
+function displayForecast (response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ` <div class = "row">`;
@@ -44,7 +45,12 @@ forecastHTML =
            forecastElement.innerHTML = forecastHTML;
            
 }
-
+function getForecast(coordinates){
+    let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature (response){
   
     let temperatureElement = document.querySelector("#temperature");
@@ -68,6 +74,7 @@ cityElement.innerHTML = response.data.name;
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
  iconElement.setAttribute("alt",response.data.weather[0].description) ;  
     
+ getForecast(response.data.coord);
     }
 
      function search(city){
