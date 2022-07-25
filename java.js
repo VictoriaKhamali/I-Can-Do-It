fd0f824025fd9005c7ac40351f0d66f07a5e3e68
VictoriaKhamali <107392444+VictoriaKhamali@users.noreply.github.com>
2022-07-25 09:51:30 +0300

    Update java.js

--- java.js
+++ java.js
@@ -47,8 +47,7 @@ forecastHTML =
 }
 function getForecast(coordinates){
     let apiKey = "c0dcf5fcd110cf002c1b9db9b7fb417e";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature (response){
